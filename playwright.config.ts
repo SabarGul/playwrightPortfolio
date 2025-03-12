98aae24f68689fb517d60d2089040c6eec1a1630
Yousaf-NewWebOrder <yousaf@newweborder.co>
2025-03-12 17:57:20 +0500

    config Updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { open: 'never' }],
+    ['json', { outputFile: 'playwright-report/report.json' }]
+  ],
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     // Run tests in headed mode
